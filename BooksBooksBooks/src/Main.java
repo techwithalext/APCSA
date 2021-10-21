47661e345cfe37d5bd2abd8d1254fb99078f11b7
Alex T <alex@alextannenbaum.com>
2021-10-21 16:41:29 -0400

    more work on BooksBooksBooks

--- BooksBooksBooks/src/Main.java
+++ BooksBooksBooks/src/Main.java
@@ -7,6 +7,13 @@ public class Main {
         Book samsungFridgeUserManual = new Book("Samsung Fridge User Manual", "Samsung", "User Manuals", 50);
         Book weaponsOfMathDestruction = new Book("Weapons of Math Destruction", "Cathy O'Neil", 321);
 
+        int totalPages = ofMiceAndMen.totalPages + lordOfTheFlies.totalPages + samsungFridgeUserManual.totalPages + weaponsOfMathDestruction.totalPages;
+        int totalTitleChars = ofMiceAndMen.bookTitle.length() + lordOfTheFlies.bookTitle.length() + samsungFridgeUserManual.bookTitle.length() + weaponsOfMathDestruction.bookTitle.length();
+        String firstLetterAuthorName = ofMiceAndMen.author.substring(0, 1) + lordOfTheFlies.author.substring(0, 1) + samsungFridgeUserManual.author.substring(0, 1) + weaponsOfMathDestruction.author.substring(0, 1);
+        String lastLetterBookTitle = ofMiceAndMen.bookTitle.substring(ofMiceAndMen.bookTitle.length() - 1) + lordOfTheFlies.bookTitle.substring(lordOfTheFlies.bookTitle.length() - 1) + samsungFridgeUserManual.bookTitle.substring(samsungFridgeUserManual.bookTitle.length() - 1) + weaponsOfMathDestruction.bookTitle.substring(weaponsOfMathDestruction.bookTitle.length() - 1);
+
+
+
         System.out.println(ofMiceAndMen.bookTitle + " by " + ofMiceAndMen.author);
         System.out.println("  - Genre: " + ofMiceAndMen.genre);
         System.out.println("  - Number of Pages: " + ofMiceAndMen.totalPages);
@@ -28,6 +35,12 @@ public class Main {
         System.out.println("  - Number of Pages: " + weaponsOfMathDestruction.totalPages);
         System.out.println("  - Ratings: " + + weaponsOfMathDestruction.getRating() + " stars, " + weaponsOfMathDestruction.getRating() + " stars, " + weaponsOfMathDestruction.getRating() + " stars");
         System.out.println();
+
+        System.out.println("Total Pages: " + totalPages);
+        System.out.println("Total Title Characters: " + totalTitleChars);
+        System.out.println("First Letters of Author's Names: " + firstLetterAuthorName);
+        System.out.println("Last Letters of Book Titles: " + lastLetterBookTitle);
+
 
 
     }
